c4ebb843194924d9eaac2fd6114d5c112233b923
HackerZ <hackerzgz@gmail.com>
2018-05-19 23:50:55 +0800

    add prepare suppport

---
+++ proxy/stmt.go
@@ -0,0 +1,13 @@
+package proxy
+
+import (
+	"database/sql"
+)
+
+// Prepare sending raw SQL first and passing args only
+// must close stmt manually after using Prepare DB connection
+func (p *Proxy) Prepare(sql string) (stmt *sql.Stmt, err error) {
+	db := p.Master
+
+	return db.Prepare(sql)
+}
